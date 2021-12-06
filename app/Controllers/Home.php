d5b83e6ebafd26f16c9d7c7b85ab8783956d0b90
IzumiShaka-desu <shakaaji29@gmail.com>
2021-12-06 19:30:42 +0700

    add session checker

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
+        if (session()->get('user') == null) {
+            return redirect()->to('/login');
+        }
         echo view('header');
-        echo view('home');
+        echo view('home_view');
         echo view('footer');
         // return view('welcome_message');
     }
