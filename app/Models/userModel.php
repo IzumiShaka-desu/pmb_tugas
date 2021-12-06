369a92761b7be60d439ad7cdced72c8b5014394f
Fikhhz <95403285+Fikhhz@users.noreply.github.com>
2021-12-06 16:04:07 +0700

    Add files via upload

---
+++ app/Models/userModel.php
@@ -0,0 +1,21 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModels extends Model
+{
+    public function getUserList()
+    {
+        return $this->db->table('identitas_motor')->getWhere([])->getResultArray();
+    }
+    public function addUser($identitas_motor)
+    {
+        return $this->db->table('identitas_motor')->insert($identitas_motor);
+    }
+    public function getUserById($id){
+            return $this->db->table('identitas_motor')->getWhere(["ID"=>$id])->getResultArray();
+        }
+
+}
+
+?>
