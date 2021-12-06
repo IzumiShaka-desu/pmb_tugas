d36476fec443db8d26046adac196393918d17467
IzumiShaka-desu <shakaaji29@gmail.com>
2021-12-06 19:36:50 +0700

    clean up ui

--- app/Views/userView.php
+++ app/Views/userView.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,45 +9,50 @@
     <title>Halaman User</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <?php include 'navbar.php';?>
+    <?php include 'header.php'; ?>
     <div class="top-wrapper">
-    <div class="container">
-    <h1 id="gudangdata" class="display-3"> Tabel User </h1>
-    <!--<form action="tambah_data.php" method="POST">-->
-<table class="table table-hover">
-    <a class="btn btn-primary" href="<?= base_url('/userController/createUser') ?>">Tambah</a>
-    <thead class="thead-dark">
-    <tr class="text-center">
-        <th scope="col">ID User</th>
-        <th scope="col">Nama</th>
-        <th scope="col">Hak Akses</th>
-        <th scope="col">Manager</th>
-        <th scope="col">Tanggal Buat</th>
-        <th scope="col">Aksi</th>
-    </tr>
-</thead>
-<tbody class="text-center">
-<div class="tabledata">
-<?php
-    foreach ($user as $item) {
-?>
-<!--query untuk menampilkan Data User-->
-    <tr>
-    <td><?= $item['IDUser'] ?></td>
-    <td><?= $item['Nama'] ?></td>
-    <td><?= $item['Hak_Akses'] ?></td>
-    <td><?= $item['Manager'] ?></td>
-    <td><?= $item['Create_Date'] ?></td>
-    <!--Tombol Edit dan hapus barang -->  
-      <td>  <a class="btn btn-sm btn-outline-secondary" href="<?= base_url('/userController/editUser/'.$item['IDUser']) ?>">Edit</a> |
-      <a href="<?= base_url('userController/delete/'.$item['IDUser']) ?>"class="btn btn-sm btn-outline-danger">Hapus</a>
-    </tr>
-    <?php   
-    } ?>
-</tbody>
-</table>
-<script>
-</script>
+        <div class="container">
+            <h1 id="gudangdata" class="display-3"> Tabel User </h1>
+            <!--<form action="tambah_data.php" method="POST">-->
+            <table class="table table-bordered table-strip table-hover table-xs-responsive">
+                <a class="btn btn-primary" href="<?= base_url('/user/createUser') ?>">Tambah</a>
+                <thead class="thead-dark">
+                    <tr class="text-center">
+                        <th scope="col">ID User</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Hak Akses</th>
+                        <th scope="col">Manager</th>
+                        <th scope="col">Tanggal Buat</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <div class="tabledata">
+                        <?php
+                        foreach ($user as $item) {
+                        ?>
+                            <!--query untuk menampilkan Data User-->
+                            <tr>
+                                <td><?= $item['IDUser'] ?></td>
+                                <td><?= $item['Nama'] ?></td>
+                                <td><?= $item['Hak_Akses'] ?></td>
+                                <td><?= $item['Manager'] ?></td>
+                                <td><?= $item['Create_Date'] ?></td>
+                                <!--Tombol Edit dan hapus barang -->
+                                <td> <a class="btn btn-sm btn-outline-secondary" href="<?= base_url('/user/editUser/' . $item['IDUser']) ?>">Edit</a> |
+                                    <a href="<?= base_url('user/delete/' . $item['IDUser']) ?>" class="btn btn-sm btn-outline-danger">Hapus</a>
+                            </tr>
+                        <?php
+                        } ?>
+                </tbody>
+            </table>
+            <?php include 'footer.php'; ?>
+
+            <script>
+            </script>
+
 </body>
+
 </html>
