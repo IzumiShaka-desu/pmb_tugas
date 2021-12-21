358c67ce75b08825bd4e0135e61a70a461a66a7b
IzumiShaka-desu <shakaaji29@gmail.com>
2021-12-21 21:17:30 +0700

    are

--- app/Models/TransaksiPenjualanModel.php
+++ app/Models/TransaksiPenjualanModel.php
@@ -8,7 +8,8 @@ class TransaksiPenjualanModel extends Model
 {
     public function getTransactionsList()
     {
-        return $this->db->table('Transaksi_Penjualan')->getWhere([])->getResultArray();
+        $query = "SELECT Transaksi_Penjualan.*,Customer.*,Identitas_Motor.* FROM `Transaksi_Penjualan` INNER JOIN Customer on Customer.Id_Cust=Transaksi_Penjualan.Id_cust INNER JOIN Identitas_Motor on Transaksi_Penjualan.Id_Kendaraan=Identitas_Motor.ID";
+        return $this->db->query($query)->getResultArray();
     }
-//    z
+    //    z
 }
