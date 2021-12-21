82ea5a44bef9a83f6b70d9329a57be440c46d8f5
IzumiShaka-desu <shakaaji29@gmail.com>
2021-12-21 22:20:14 +0700

    add transaction

--- app/Controllers/Transaction.php
+++ app/Controllers/Transaction.php
@@ -2,16 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\TransaksiPenjualanModel;
+
 class Transaction extends BaseController
 {
+    public function __construct()
+    {
+        $this->uModel = new TransaksiPenjualanModel();
+    }
+
     public function index()
     {
         if (session()->get('user') == null) {
             return redirect()->to('/login');
         }
 
+        $data["transaksi_motor"] = $this->uModel->getTransactionsList();
         echo view('header');
-echo view('transaksiView');
+        echo view('transaksiView', $data);
         echo view('footer');
         // return view('welcome_message');
     }
