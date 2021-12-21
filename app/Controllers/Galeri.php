1b4dca88ab1656ffbd85fe921495baca188d6988
IzumiShaka-desu <shakaaji29@gmail.com>
2021-12-21 22:20:02 +0700

    add gallery motor

--- app/Controllers/Galeri.php
+++ app/Controllers/Galeri.php
@@ -10,20 +10,21 @@ class Galeri extends BaseController
     {
         $this->uModel = new IdentitasMotorModel();
     }
-    
+
     public function index()
     {
         if (session()->get('user') == null) {
             return redirect()->to('/login');
         }
         echo view('header');
+
         $data["identitas_motor"] = $this->uModel->getUserList();
         /*for($x = 0; $x <= 10; $x++){
             array_push($data["identitas_motor"], $data["identitas_motor"][0] );
 
-        }*/ 
-        
-        echo view('GaleriMotorView',$data);
+        }*/
+
+        echo view('GaleriMotorView', $data);
         echo view('footer');
         // return view('welcome_message');
     }
